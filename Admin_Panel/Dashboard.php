d4c24b000cf0bedd214150c6b4296cd6b9495b8d
jankikansagra03 <122074631+jankikansagra03@users.noreply.github.com>
2025-03-10 13:23:27 +0530

    demo

--- Admin_Panel/Dashboard.php
+++ Admin_Panel/Dashboard.php
@@ -52,7 +52,7 @@ include('index.php');
                 <div class="col-12 col-md-6 col-lg-4">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">Total Users</h5>
+                            <h5 class="card-title">Total Users demo</h5>
                             <div class="card-value">1,234</div>
                         </div>
                     </div>
